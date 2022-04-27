b39bff7b7c1c94120d869642927689ef74a6ca5e
Asha Schwegler <schweglerasha@gmail.com>
2022-04-27 15:23:11 +0200

    refactor doc2

---
+++ src/Tests/TestGeneratePrimes.java
@@ -0,0 +1,40 @@
+package Tests;
+
+import junit.framework.TestCase;
+
+import junit.textui.TestRunner;
+import main.java.PrimeGenerator;
+
+
+
+
+
+
+
+public class TestGeneratePrimes extends TestCase {
+    public static void main(String args[]){
+        TestRunner.main(new String[] {"TestGeneratePrimes"});
+    }
+
+    public TestGeneratePrimes(String name){
+        super(name);
+    }
+
+    public void testPrimes(){
+        int[] nullArray = PrimeGenerator.generatePrimes(0);
+        assertEquals(nullArray.length, 0);
+        int[] minArray = PrimeGenerator.generatePrimes(2);
+        assertEquals(minArray.length, 1);
+        assertEquals(minArray[0], 2);
+        int[] threeArray = PrimeGenerator.generatePrimes(3);
+        assertEquals(threeArray.length, 2);
+        assertEquals(threeArray[0], 2);
+        assertEquals(threeArray[1], 3);
+        int[] centArray = PrimeGenerator.generatePrimes(100);
+        assertEquals(centArray.length, 25);
+        assertEquals(centArray[24], 97);
+
+    }
+
+
+}
---
+++ src/main/java/PrimeGenerator.java
@@ -0,0 +1,107 @@
+package main.java;
+
+
+public class PrimeGenerator {
+    private static boolean[] isCrossed;
+    private static int[] result;
+
+
+public static int [] generatePrimes(int maxValue) {
+        if (maxValue < 2)
+            return new int[0];
+        else{
+            initializeArrayOfIntegers(maxValue);
+            crossOutMultiples();
+            putUncrossedIntegersIntoResult();
+            return result;
+        }
+}
+    private static void initializeArrayOfIntegers(int maxValue){
+        isCrossed = new boolean[maxValue+1];
+        for (int i = 2; i < isCrossed.length; i++) {
+            isCrossed[i] = false;
+        }
+    }
+    private static void crossOutMultiples(){
+        int maxPrimeFactor = calcMaxPrimeFactor();
+        for (int i = 2; i <= maxPrimeFactor; i++) {
+            if (notCrossed(i)) {
+               crossOutMultiplesOf(i);
+            }
+        }
+
+    }
+
+private static int calcMaxPrimeFactor(){
+    double maxPrimeFactor = Math.sqrt(isCrossed.length) + 1;
+    return (int) maxPrimeFactor;
+}
+
+
+private static void crossOutMultiplesOf(int i){
+    for(int multiple = 2*i; multiple < isCrossed.length; multiple +=i){
+        isCrossed[multiple] = true;
+    }
+}
+
+private static boolean notCrossed(int i){
+    return isCrossed[i] == false;
+}
+
+private static void putUncrossedIntegersIntoResult(){
+    int i;
+    int j;
+    //Count number of primes
+    int count = 0;
+    for (i = 0; i < isCrossed.length; i++) {
+        if (isCrossed[i]) {
+            count++;
+        }
+        int[] primeNumbers = new int[count];
+
+        for (i = 0, j = 0; i < isCrossed.length; i++) {
+            if (isCrossed[i]) {
+                primeNumbers[j++] = i;
+            }
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
